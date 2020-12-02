6f1f2897ca05b31aa33999bb395bdd57308edae6
Muhammad Soleh <matthelosh@gmail.com>
2020-12-02 10:19:01 +0700

    Add foto pak awal

--- resources/views/pages/operator/home.blade.php
+++ resources/views/pages/operator/home.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-12">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title">Halo {{ Auth::user()->level }}</h4>
+            <h4 class="card-title">Halo {{ Auth::user()->nama }}</h4>
         </div>
     </div>
 </div>
